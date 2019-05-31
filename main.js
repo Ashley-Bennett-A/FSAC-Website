0124cebf6259072a687ea6a5f5e997667d6a8189
CN0072 <CN0072@CN0072.lan>
2019-05-31 17:15:16 +0100

    hamburger

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const displayNav = () => {
-  var x = document.getElementById("myLinks");
+  let x = document.getElementById("myLinks");
+  let burger = document.getElementById("button")
   if (x.style.display === "block") {
     x.style.display = "none";
+    burger.classList.remove("is-active")
+    
   } else {
     x.style.display = "block";
+    burger.classList.add("is-active")
+    x.classList.add("is-active")
   }
 }
