d85b6a2fef607da9d62407b796e615586f63e9e2
CN0072 <CN0072@CN0072.lan>
2019-06-04 10:32:49 +0100

    basic nav animation

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const displayNav = () => {
   let burger = document.getElementById("button")
   if (links.style.display === "block") {
     links.style.display = "none";
+    links.classList.remove("animation")
     burger.classList.remove("is-active")
     
   } else {
     links.style.display = "block";
     burger.classList.add("is-active")
+    links.classList.add("animation")
   }
 }
